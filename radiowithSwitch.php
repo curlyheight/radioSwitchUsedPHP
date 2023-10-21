c67cf62315753700fa71b90d0bd9c8397a17ad21
curlyheight <148594403+curlyheight@users.noreply.github.com>
2023-10-21 07:21:54 +0600

    Add files via upload

---
+++ radiowithSwitch.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="real.php" method="post">
+        <input type="radio" name="credit_card"  value="Visa">Visa <br>
+        <input type="radio" name="credit_card"  value="Mastercard">MasterCard <br>
+        <input type="radio" name="credit_card"  value="American Express">American Express <br>
+        <input type="submit" name="confirm" value="confirm">
+    </form>
+    
+</body>
+</html>
+
+<?php
+    //    if(isset($_POST["confirm"] )){
+    //         if(isset($_POST["credit_card"])){
+    //             $credit_card = $_POST["credit_card"];
+    //             echo $credit_card;
+    //         }
+    //         else{
+    //             echo"Please make selection";
+    //         }
+
+    //    }
+
+
+    if(isset($_POST["confirm"] )){
+                $credit_card = null;
+
+                if(isset($_POST["credit_card"])){
+                    $credit_card = $_POST["credit_card"];
+                }
+                
+                switch($credit_card){
+                    case"Visa":
+                        echo"You selected Visa";
+                        break;
+                    case"Mastercard":
+                        echo"You selected Master Card";
+                        break;
+                    case"American Express":
+                        echo"You selected American Express";
+                        break;
+                    default:
+                        echo"<b> Please make a selection";
+                    
+                }
+
+    
+           }
+?>
